c44b7e8bb32b9552c5969460a730e8ecb3810387
Yeying-Xingchen <yeyingxingchen@yeah.net>
2025-12-08 00:16:38 +0800

    feat: :sparkles: 处理接收到的入群邀请，好友申请

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
 """程序总入口"""
 
+from contextlib import asynccontextmanager
+import importlib
 from fastapi import FastAPI
 from plugins import github, oseddl, bilibili
+import tomllib
 
 app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("CodeFeatrue-破晓之码 正在启动")
+    with open("config.toml", "rb") as f:
+        _config_data = tomllib.load(f)
+    for plugin_name in _config_data["main"]["plugins"]:
+        # 初始化插件
+        plugin = importlib.import_module(plugin_name)
+        try:
+            plugin.on_enable(app)
+        except AttributeError:
+            pass
+        except Exception as e:
+            print(f"插件 {plugin_name} 启动失败: {e}")
+        pass
+    yield
+    print("CodeFeatrue-破晓之码 正在退出")
 
 @app.post("/")
 def main(info: dict):
